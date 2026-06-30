327b51350dc648a48f7c2a711c48916d37fc9633
Jose Bacilio <74934503@continental.edu.pe>
2026-06-30 13:46:45 -0500

    feat: restore 'cómo llegar a la tienda' navigation item for client role

--- lib/presentation/widgets/common/app_drawer.dart
+++ lib/presentation/widgets/common/app_drawer.dart
@@ -165,6 +165,13 @@ class AppDrawer extends ConsumerWidget {
                   ),
                   _drawerItem(
                     context,
+                    label: 'CÓMO LLEGAR A LA TIENDA',
+                    icon: Icons.explore_rounded,
+                    isSelected: currentRoute == 'gps',
+                    onTap: () => _navigate(context, MapaRutaScreen(usuarioId: user.id)),
+                  ),
+                  _drawerItem(
+                    context,
                     label: 'NIVELADOR DIGITAL ALY',
                     icon: Icons.architecture_rounded,
                     isSelected: currentRoute == 'sensor_level',
